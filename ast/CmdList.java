2bdc8150ad76a8dc8fd8485156e2ff63d7571e20
liliozorio <liviapozorio@gmail.com>
2023-06-04 17:22:28 -0300

    testando

--- ast/CmdList.java
+++ ast/CmdList.java
@@ -32,8 +32,9 @@ public class CmdList extends Node {
             try
             {
                 Boolean run_return = (Boolean)o;
-                if(n.getClass().getSimpleName().equals("ReturnCMD") || run_return)
+                if((n.getClass().getSimpleName().equals("ReturnCMD") || run_return) && !(n.getClass().getSimpleName().equals("Bool")))
                 {
+                    System.out.println("AKI");
                     return true;
                 }
             }
