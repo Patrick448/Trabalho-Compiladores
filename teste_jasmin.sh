565b29cd5aaefab817658ddf84afd090022fdb7d
liliozorio <liviapozorio@gmail.com>
2023-07-09 15:35:08 -0300

    Adiciona anotações

---
+++ teste_jasmin.sh
@@ -0,0 +1,59 @@
+echo
+echo "--------------------TESTES CORRETOS--------------------"
+echo
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste0.lan -j -v
+javac -cp JavaCodes JavaCodes/teste0.j
+java -cp JavaCodes teste0
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste1.lan -j -v
+javac -cp JavaCodes JavaCodes/teste1.j
+java -cp JavaCodes teste1
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste1eMeio.lan -j -v
+javac -cp JavaCodes JavaCodes/teste1eMeio.j
+java -cp JavaCodes teste1eMeio
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste2.lan -j -v
+javac -cp JavaCodes JavaCodes/teste2.j
+java -cp JavaCodes teste2
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste3.lan -j -v
+javac -cp JavaCodes JavaCodes/teste3.j
+java -cp JavaCodes teste3
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste4.lan -j -v
+javac -cp JavaCodes JavaCodes/teste4.j
+java -cp JavaCodes teste4
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste5.lan -j -v
+javac -cp JavaCodes JavaCodes/teste5.j
+java -cp JavaCodes teste5
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste6.lan -j -v
+javac -cp JavaCodes JavaCodes/teste6.j
+java -cp JavaCodes teste6
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste7.lan -j -v
+javac -cp JavaCodes JavaCodes/teste7.j
+java -cp JavaCodes teste7
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste8.lan -j -v
+javac -cp JavaCodes JavaCodes/teste8.j
+java -cp JavaCodes teste8
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste9.lan -j -v
+javac -cp JavaCodes JavaCodes/teste9.j
+java -cp JavaCodes teste9
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste10.lan -j -v
+javac -cp JavaCodes JavaCodes/teste10.j
+java -cp JavaCodes teste10
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste11.lan -j -v
+javac -cp JavaCodes JavaCodes/teste11.j
+java -cp JavaCodes teste11
+
+java -cp .:antlr-4.8-complete.jar LangCompiler testes/semantica/certo/teste12.lan -j -v
+javac -cp JavaCodes JavaCodes/teste12.j
+java -cp JavaCodes teste12
